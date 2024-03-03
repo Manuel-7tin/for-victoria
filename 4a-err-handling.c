6fa88468bc8a20b38a6ff34efeda644cfb0f550d
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2024-03-03 16:36:11 +0100

    Create 4a-err-handling.c

---
+++ 4a-err-handling.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+//declares a macro to hold the value of expeted number of marks
+#define MAX_SCORES 60
+
+int main(void){
+  int scores[MAX_SCORES];
+  int score_sum = 0;
+  float average_score;
+  int i = 0;
+
+  // Taking input for 60 numbers
+  printf("Enter 60 exam scores separated by spaces:\n");
+  for (;i < MAX_SCORES; i++) {
+      scanf("%d", &scores[i]);
+    if (getchar() == '\n') {
+        i++;
+        break;
+    }
+  }
+
+  // Output error message
+  if (i < MAX_SCORES){
+    printf("Note: You entered less than %d scores.\n", MAX_SCORES);
+    printf("You entered %d instead of %d scores.\n", i, MAX_SCORES);
+  }
+  
+  // Calculates and outputs sum of all scores in the array
+  for (int j = 0; j < i; j++) 
+      score_sum += scores[j];
+  printf("The sum total of the score is:  %d\n", score_sum);
+
+  //Calculates and outputs the average score
+  average_score = (float)score_sum / i;
+  printf("The average score is:  %.2f\n", average_score);
+
+  return 0;
+}
